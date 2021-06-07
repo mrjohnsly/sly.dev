e2efbe3d491dbeebaad46a5e49373ccb795bcb97
John Sylvester <john@sly.dev>
2021-06-07 15:19:34 +0100

    Add date and time filters

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,19 @@
+const { DateTime } = require("luxon");
+
 module.exports = function(eleventyConfig) {
+  
+  eleventyConfig.addFilter("readableDate", dateObj => {
+    return DateTime.fromJSDate(dateObj, {zone: "Europe/London"}).toFormat("dd LLLL yyyy");
+  });
+
+  eleventyConfig.addFilter("readableTime", dateObj => {
+    return DateTime.fromJSDate(dateObj, {zone: "Europe/London"}).toFormat("HH:mm");
+  });
+
+  eleventyConfig.addFilter("isoDate", dateObj => {
+    return DateTime.fromJSDate(dateObj, {zone: "Europe/London"}).toISO();
+  });
+
   return {
     markdownTemplateEngine: 'njk',
     dataTemplateEngine: 'njk',
