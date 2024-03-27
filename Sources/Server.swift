abe697b8caf2e5b7a59ab363411012ff5ddd68d1
John Sylvester <john@sly.dev>
2024-03-27 07:22:58 +0000

    Add /api endpoint

--- Sources/Server.swift
+++ Sources/Server.swift
@@ -5,10 +5,30 @@
 // https://swiftpackageindex.com/apple/swift-argument-parser/documentation
 
 import ArgumentParser
+import Hummingbird
 
 @main
-struct Server: ParsableCommand {
-    mutating func run() throws {
-        print("Hello, world!")
+struct Server: AsyncParsableCommand {
+    
+    func run() async throws {
+        let app = try await buildApplication()
+        try await app.runService()
     }
+    
+}
+
+func buildApplication() async throws -> some ApplicationProtocol {
+    
+    let router = Router()
+    router.get("/api") { request, context in
+        "Up!\n"
+    }
+    
+    let app = Application(
+        router: router,
+        configuration: .init(address: .hostname("127.0.0.1", port: 8080))
+    )
+    
+    return app
+    
 }
--- Tests/ServerTests.swift
+++ Tests/ServerTests.swift
@@ -1,6 +1,17 @@
 @testable import Server
+import Hummingbird
+import HummingbirdTesting
 import XCTest
 
 final class ServerTests: XCTest {
+    
+    func test_Get_Api_RespondsWithStatus200() async throws {
+        let app = try await buildApplication()
+        try await app.test(.router) { client in
+            try await client.execute(uri: "/api", method: .get) { response in
+                XCTAssertEqual(response.status, .ok)
+            }
+        }
+    }
     
 }
