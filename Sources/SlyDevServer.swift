ffc434424481b495d6de85eec9b8dc0f0dffa66e
John Sylvester <john@sly.dev>
2024-04-19 20:28:52 +0100

    Add command line parameters

--- Sources/SlyDevServer.swift
+++ Sources/SlyDevServer.swift
@@ -11,12 +11,18 @@ import Mustache
 
 @main
 struct SlyDevServer: AsyncParsableCommand {
+    @Option(name: .shortAndLong)
+    var hostname: String = "127.0.0.1"
+
+    @Option(name: .shortAndLong)
+    var port: Int = 8080
+
     func run() async throws {
         var logger = Logger(label: "SlyDevServer")
         logger.logLevel = .debug
 
         let library = try await MustacheLibrary(directory: "templates")
-        
+
         let router = Router()
         router.middlewares.add(LogRequestsMiddleware(.info))
         router.get("/") { request, context -> HTML in
@@ -28,7 +34,11 @@ struct SlyDevServer: AsyncParsableCommand {
             "Up!"
         }
 
-        let app = Application(router: router, logger: logger)
+        let app = Application(
+            router: router,
+            configuration: .init(address: .hostname(self.hostname, port: self.port)),
+            logger: logger
+        )
 
         try await app.runService()
     }
