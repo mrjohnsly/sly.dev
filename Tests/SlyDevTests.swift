eda7717084b236324544f9ef9f6aaa7a29ebc2b9
John Sylvester <john@sly.dev>
2024-04-20 07:32:28 +0100

    Add TestArguments

--- Tests/SlyDevTests.swift
+++ Tests/SlyDevTests.swift
@@ -2,6 +2,11 @@
 import XCTest
 
 final class SlyDevServerTests: XCTestCase {
+	struct TestArguments: AppArguments {
+		var hostname: String = "127.0.0.1"
+		var port: Int = 8080
+	}
+
 	func test_Example() {
 		XCTAssertTrue(true)
 	}
